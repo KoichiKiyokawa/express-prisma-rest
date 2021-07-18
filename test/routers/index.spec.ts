8fd783415f5e1afdc5d2e549a08864c94d7820a5
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-18 16:39:15 +0900

    test: remove supertest because test never end in ci

--- test/routers/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import supertest from "supertest";
-import { app } from "../../src";
-
-describe("router test", () => {
-  it("index", async () => {
-    const res = await supertest(app).get("/");
-    expect(res.statusCode).toBe(200);
-  });
-});
