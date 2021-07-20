173d50bec2ee9dc9cced04df3dc61ec5a036cacc
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-20 13:30:27 +0900

    chore: disable minify

--- bin/base-options.ts
+++ bin/base-options.ts
@@ -3,7 +3,6 @@ import { BuildOptions } from "esbuild";
 export const baseOption: BuildOptions = {
   bundle: true,
   entryPoints: ["src/index.ts"],
-  minify: true,
   outdir: "dist",
   platform: "node",
   sourcemap: true,
