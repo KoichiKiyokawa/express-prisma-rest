c827e06e706d72bd6d45f4320e86140d283a74a7
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-21 23:39:33 +0900

    exp

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,31 @@ import session from "fastify-session";
 import redis from "redis";
 import { setupRoutes } from "./domains/app/router";
 
-const isProd = process.env.NODE_ENV === "production";
+async function bootstrap() {
+  const isProd = process.env.NODE_ENV === "production";
 
-const RedisStore = connectRedis(session as any);
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST ?? "localhost",
-  port: Number(process.env.REDIS_PORT) ?? 6379,
-});
+  const RedisStore = connectRedis(session as any);
+  const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST ?? "localhost",
+    port: Number(process.env.REDIS_PORT) ?? 6379,
+  });
 
-const app = fastify({ logger: { prettyPrint: !isProd } });
+  const app = fastify({ logger: { prettyPrint: !isProd } });
 
-// TODO: CHANGE IN PRODUCTION
-app.register(cors, { credentials: true, origin: "http://localhost:3000" });
-app.register(cookie);
-// TODO: CHANGE IN PRODUCTION
-app.register(session, {
-  secret: "eoiajonlkntoaierngoangnlkanekrgaeoijm;mkda",
-  cookie: { secure: isProd },
-  store: new RedisStore({ client: redisClient }),
-});
+  // TODO: CHANGE IN PRODUCTION
+  app.register(cors, { credentials: true, origin: "http://localhost:3000" });
+  app.register(cookie);
+  // TODO: CHANGE IN PRODUCTION
+  app.register(session, {
+    secret: "eoiajonlkntoaierngoangnlkanekrgaeoijm;mkda",
+    cookie: { secure: isProd },
+    store: new RedisStore({ client: redisClient }),
+  });
 
-setupRoutes(app);
+  setupRoutes(app);
 
-const port = process.env.PORT ?? 8080;
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+  const port = process.env.PORT ?? 8080;
+  app.listen(port, () => console.log(`App listening on port ${port}!`));
+}
+
+bootstrap().catch(console.error);
