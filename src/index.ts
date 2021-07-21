de935383003a0cab0dce9b1d15325210bebdb17e
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-21 22:22:14 +0900

    chore: show console on start

--- src/index.ts
+++ src/index.ts
@@ -28,4 +28,5 @@ app.register(session, {
 
 setupRoutes(app);
 
-app.listen(process.env.PORT ?? 8080);
+const port = process.env.PORT ?? 8080;
+app.listen(port, () => console.log(`App listening on port ${port}!`));
