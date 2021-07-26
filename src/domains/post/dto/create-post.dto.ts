790ea79bd93e8627c92e87ee3c9ff65aa309b5a9
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-26 23:30:48 +0900

    chore: set additionalProperties to false

--- src/domains/post/dto/create-post.dto.ts
+++ src/domains/post/dto/create-post.dto.ts
@@ -1,8 +1,11 @@
 import { Static, Type } from "@sinclair/typebox";
 
-export const CreatePostDto = Type.Object({
-  title: Type.String(),
-  body: Type.String(),
-});
+export const CreatePostDto = Type.Object(
+  {
+    title: Type.String(),
+    body: Type.String(),
+  },
+  { additionalProperties: false }
+);
 
 export type TCreatePostDto = Static<typeof CreatePostDto>;
